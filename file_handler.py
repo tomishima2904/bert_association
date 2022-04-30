814be3a876afa80b3b83d8325d783df4a94ebc86
tomishima <pumoco2904@icloud.com>
2022-04-30 21:29:38 +0900

    file_handerにdir_name_getterを追加

--- file_handler.py
+++ file_handler.py
@@ -2,6 +2,7 @@ import csv
 import ast
 import os
 import pandas as pd
+import datetime
 
 
 # results_csvを読み込む関数
@@ -20,3 +21,31 @@ def csv_writer(header:list, result:list, csv_file_path:str):
       writer = csv.writer(f)
       writer.writerow(header)
       writer.writerows(result)
+
+
+def dir_name_getter(args, get_date=None):
+    # この処理に共通する保存パス
+    if type(get_date) is str:
+        date_time = get_date
+
+    else:       
+        t_delta = datetime.timedelta(hours=9)
+        JST = datetime.timezone(t_delta, 'JST')
+        now = datetime.datetime.now(JST)
+        date_time = now.strftime('%y%m%d_%H%M%S')
+
+    # 出力するディレクトリ名を決めるための処理
+    if args.brackets_flag: brackets = "brkt"
+    else: brackets = "WObrkt"
+
+    if args.another_analysis == 293: another_name = "anl"
+    else: another_name = "WOanl"
+
+    if args.multi_stims_flag: stims_name = "stims{}".format(args.num_stims)
+    else: stims_name = "WOstims"
+
+    if args.category_flag: cat_name = "cat"
+    else: cat_name = "WOcat"
+
+    save_dir = f"results/{date_time}_{args.max_words}_{brackets}_{another_name}_{args.model_opt}_{stims_name}_{cat_name}_{args.extract_noun_opt}_{args.eval_opt}/"      
+    return save_dir
